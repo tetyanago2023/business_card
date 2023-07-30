15d97853208402061cd4e2176c960b7b0b0203ec
go2023 <tetyanago2018@gmail.com>
2023-07-29 19:00:47 -0700

    gh-pages data was removed

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/business_card_gh/",
-  plugins: [react()],
-})
+// export default defineConfig({
+//   base: "/business_card_gh/",
+//   plugins: [react()],
+// })
